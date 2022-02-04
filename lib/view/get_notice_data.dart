390b5533c36d84fa6b6fb8a7457b8f34309088b4
savan <savanitaliya11@gmail.com>
2022-02-04 10:44:33 +0530

    swagger api integration and delete data successfull

--- lib/view/get_notice_data.dart
+++ lib/view/get_notice_data.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
 
-
 import 'package:flutter/material.dart';
 import 'package:swagger_api_integration/api/model/res_model/note_list_res_model.dart';
 import 'package:swagger_api_integration/api/repo/delete_notelist_user.dart';
